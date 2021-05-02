ddaab4d98c7e3eb6687cd529d2433d018fd7808f
James Alseth <james@jalseth.me>
2021-05-02 11:35:23 -0700

    Don't include timestamp in CLI docs generation
    There's no reason to update the CLI docs if the only difference
    is the timestamp for when they are generated.

--- .github/gendocs/konstraint.go
+++ .github/gendocs/konstraint.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	konstraint := commands.NewDefaultCommand()
+	konstraint.DisableAutoGenTag = true
 	if err := doc.GenMarkdownTree(konstraint, "./docs/cli"); err != nil {
 		log.Fatal(err)
 	}
